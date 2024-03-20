e4b8687915f5a5ca8f9f35b25aadbd56d031643c
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-03-20 18:47:41 +1300

    Express server is working, don't know the correct way of intergrating it with Next server.

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
